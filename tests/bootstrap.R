c970490682342fe2a7b767040698d8d34a25eb9f
Ronald Hochreiter <ron@hochreiter.net>
2014-09-25 12:13:19 +0200

    minor fix

--- tests/bootstrap.R
+++ tests/bootstrap.R
@@ -5,7 +5,7 @@ data(sp500)
 series <- sp500["2009/2012"]
 returns <- dailyReturn(series)
 
-b <- block_bootstrap(as.vector(returns), 30, 20)
+b <- block.bootstrap(as.vector(returns), 30, 20)
 
 plot(b)
 plot(return2price(b, as.numeric(Ad(series[1]))))
