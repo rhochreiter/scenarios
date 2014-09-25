03be828375506d11e5aede1e8b685774149b4c20
Ronald Hochreiter <ron@hochreiter.net>
2014-09-25 12:10:36 +0200

    added block bootstrapping

---
+++ tests/bootstrap.R
@@ -0,0 +1,11 @@
+library(scenarios)
+
+data(sp500)
+
+series <- sp500["2009/2012"]
+returns <- dailyReturn(series)
+
+b <- block_bootstrap(as.vector(returns), 30, 20)
+
+plot(b)
+plot(return2price(b, as.numeric(Ad(series[1]))))
